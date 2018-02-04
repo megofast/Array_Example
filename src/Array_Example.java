8b8655412bccd83d560930c1a71484e521f086a0
megofast <33850107+megofast@users.noreply.github.com>
2018-02-04 16:51:03 -0600

    Initial commit

---
+++ src/Array_Example.java
@@ -0,0 +1,9 @@
+public class Array_Example {
+    public static void main(String args[]) {
+        int x;
+        String ingredients[] = {"Ice cream", "Sprinkles", "Chocolate Syrup", "Caramel Syrup", "Cherries"};
+        for (x = 0; x < 5; x++) {
+            System.out.println(ingredients[x]);
+        }
+    }
+}
